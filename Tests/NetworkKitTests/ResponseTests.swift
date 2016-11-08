76b8c820327ed8555764aea42d2f10e43801fd40
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2016-11-08 18:04:36 +0100

    Add test for Response, to see if content type is set correct

---
+++ Tests/NetworkKitTests/ResponseTests.swift
@@ -0,0 +1,29 @@
+import XCTest
+@testable import NetworkKit
+
+class ResponseTests: XCTestCase {
+    var sampleHTTPResponse: HTTPURLResponse!
+    var sampleURL: URL!
+    var sampleSuccessStatusCode = 200
+    var sampleContentTypeJSONHeader = [Header.contentType: ContentType.applicationJSON.rawValue]
+    override func setUp() {
+        super.setUp()
+        sampleURL = URL(string: "http://nu.nl")!
+        sampleHTTPResponse = HTTPURLResponse(url: sampleURL,
+                                             statusCode: sampleSuccessStatusCode,
+                                             httpVersion: nil,
+                                             headerFields: sampleContentTypeJSONHeader)
+    }
+    
+    func testIfResponseHasContentType() {
+        let subject = Response(data: nil, httpResponse: sampleHTTPResponse, error: nil)
+        XCTAssertEqual(subject.contentType, ContentType.applicationJSON)
+    }
+
+    
+    static var allTests : [(String, (ResponseTests) -> () throws -> Void)] {
+        return [
+            ("testIfResponseHasCorrectContentType", testIfResponseHasContentType),
+        ]
+    }
+}
