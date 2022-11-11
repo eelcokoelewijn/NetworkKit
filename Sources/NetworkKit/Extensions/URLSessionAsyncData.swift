c4f519d846d1d200fd107e838d9372adfae512b9
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2022-11-11 21:35:25 +0100

    fix(macos): Add availability for api

--- Sources/NetworkKit/Extensions/URLSessionAsyncData.swift
+++ Sources/NetworkKit/Extensions/URLSessionAsyncData.swift
@@ -3,17 +3,17 @@ import Foundation
 import FoundationNetworking
 #endif
 
-#if canImport(FoundationNetworking)
 extension URLSession {
     /**
      Adds option for async/await to URLSession using `withUnsafeThrowingContinuation` api for Linux.
      The async URLSession APIs aren't available on FoundationNetworking
-
+     
      This doesn't have the same semantics as Darwin, because Darwin ensures task cancellation trickles down into canceling the connection, which isn't occurring here.
-
+     
      - Ref: https://medium.com/hoursofoperation/use-async-urlsession-with-server-side-swift-67821a64fa91
      - Ref: https://forums.swift.org/t/how-to-use-async-await-w-docker/49591/13
      **/
+    @available(macOS 10.15, *)
     func asyncData(for request: URLRequest) async throws -> (Data, URLResponse) {
         return try await withUnsafeThrowingContinuation { continuation in
             let sessionDataTask = self.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
@@ -30,4 +30,3 @@ extension URLSession {
         }
     }
 }
-#endif
